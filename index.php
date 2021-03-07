5887f3a5f8a7036b89b12ae9f850a24529914a47
chamodya wimansha <chamodyawimansha@gmail.com>
2021-03-07 17:03:09 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Entry file to the application
+ * php version 7.4.2
+ *
+ * @category File
+ * @package  SimplePHP
+ * @author   Chamodya Wimansha <chamodyawimansha@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/chamodyawimansha/SimplePHP
+ */
+
+ require 'framework/core/Application.php';
+
+ Application::init();
