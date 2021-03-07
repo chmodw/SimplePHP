076bf7054c0d5461f47825fdb4019ddfd93ad06f
chamodya wimansha <chamodyawimansha@gmail.com>
2021-03-07 19:37:12 +0530

    creates config.php file

---
+++ App/config/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Config File - stores application configs
+ * php version 7.4.2
+ *
+ * @category Core_Class
+ * @package  SimplePHP
+ * @author   Chamodya Wimansha <chamodyawimansha@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU Gene
+ * @link     https://github.com/chamodyawimansha/SimplePHP
+ */
+
+return [
+
+  "sample" => ROOT,
+
+];
