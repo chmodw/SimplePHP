8a1a792c0c69bfdac7c745beecb2c82649d67b2b
chamodya wimansha <chamodyawimansha@gmail.com>
2021-03-10 18:19:11 +0530

    creates the model parent class

---
+++ Framework/Core/Model.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Parent Model file
+ * php version 7.4.2
+ *
+ * @category Core_Class
+ * @package  SimplePHP
+ * @author   Chamodya Wimansha <chamodyawimansha@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU Gene
+ * @link     https://github.com/chamodyawimansha/SimplePHP
+ */
+
+namespace Framework\Core;
+
+/**
+ * Model Class - Parent class of the Model classes
+ * php version 7.4.2
+ *
+ * @category Core_Class
+ * @package  SimplePHP
+ * @author   Chamodya Wimansha <chamodyawimansha@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU Gene
+ * @link     https://github.com/chamodyawimansha/SimplePHP
+ */
+class Model
+{
+
+}
