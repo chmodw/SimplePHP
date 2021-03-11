5009e26fe75eea30129f6baae03dcd3f702b062b
chamodya wimansha <chamodyawimansha@gmail.com>
2021-03-11 19:42:26 +0530

    creates a logger class to log errors

---
+++ Framework/Core/Logger.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+ * Logger file
+ * php version 7.4.2
+ *
+ * @category Framework_Class
+ * @package  SimplePHP
+ * @author   Chamodya Wimansha <chamodyawimansha@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU Gene
+ * @link     https://github.com/chamodyawimansha/SimplePHP
+ */
+
+namespace Framework\Logger;
+
+/**
+ * Logger Class - Handles errors, logging and redirection.
+ * php version 7.4.2
+ *
+ * @category Database_Class
+ * @package  SimplePHP
+ * @author   Chamodya Wimansha <chamodyawimansha@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU Gene
+ * @link     https://github.com/chamodyawimansha/SimplePHP
+ */
+
+Class Logger{
+
+
+
+}
