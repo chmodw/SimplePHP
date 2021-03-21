87eaaa7b61edac1a8c683338cd1876366e4b6fe1
chamodya wimansha <chamodyawimansha@gmail.com>
2021-03-21 13:27:38 +0530

    installs and require the pear/mail composer package

---
+++ Framework/Libraries/ContactForm.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * ContactForm File
+ * php version 7.4.2
+ *
+ * @category Library_Class
+ * @package  SimplePHP
+ * @author   Chamodya Wimansha <chamodyawimansha@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU Gene
+ * @link     https://github.com/chamodyawimansha/SimplePHP
+ */
+
+ namespace Framework\Libraries;
+
+ require ROOT . "vendor/pear/mail/Mail.php";
+
+ use Mail;
+
+/**
+ * ContactForm Class - Uses the Pear::Mail class to send emails 
+ * php version 7.4.2
+ *
+ * @category Library_Class
+ * @package  SimplePHP
+ * @author   Chamodya Wimansha <chamodyawimansha@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU Gene
+ * @link     https://github.com/chamodyawimansha/SimplePHP
+ */
+
+class ContactForm
+{
+    /**
+     * Runs when Object creating
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+}
